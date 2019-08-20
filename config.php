72917877d78061cc9755b0ae56f80b58609e822f
LuongTham <thamltgch17164@fpt.edu.vn>
2019-08-20 23:23:58 +0700

    thay đổi file
    config

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
-<?php 
+<!-- <?php 
 	$hostname = "localhost";
 	$username = "root";
 	$password = "";
@@ -8,4 +8,11 @@
 	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	// set che do de hien thi tieng viet
 	$pdo->exec("set names utf8");
+ ?>
+ -->
+ <?php
+$connection = pg_connect("host=ec2-54-225-106-93.compute-1.amazonaws.com port=5432 dbname=d8bgahfgm7qf2o user=njsytzlwnmlnhu password=b109ec9762a8357e014891406f11fa2046d644212105010796d202df7cabb87b");  
+ if(!$connection) {
+     die("Database connection failed");
+ }
  ?>
