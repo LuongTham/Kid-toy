966199d52a111d68bb9991275ccf7d3417cf64a9
LuongTham <thamltgch17164@fpt.edu.vn>
2019-08-31 00:09:37 +0700

    hahhaha
    ádasds

--- ketnoi.php
+++ ketnoi.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = pg_connect("host=ec2-54-235-208-103.compute-1.amazonaws.com port=5432 dbname=dcpimk1h3359uq user=gwsltdzfcfttmd password=1dea06845bb4e32e02ce752470895898c470c7216db4ef3f27c71155a7b0fbcd");  
+$connection = pg_connect("host=ec2-54-225-106-93.compute-1.amazonaws.com port=5432 dbname=d8bgahfgm7qf2o user=njsytzlwnmlnhu password=b109ec9762a8357e014891406f11fa2046d644212105010796d202df7cabb87b");  
  if(!$connection) {
      die("Database connection failed");
  }
